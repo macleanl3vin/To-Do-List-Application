4623524407ab377b5c9881a70bacc8ddf396fe44
MacLean Levin <maclean@forge.eco>
2024-03-23 14:10:38 -0400

    Added a task class with getters and setters

---
+++ task/task.java
@@ -0,0 +1,46 @@
+package task;
+
+public class task {
+    private String name;
+    private String dueDate;
+    private String priorityLevel;
+
+    public task(String name, String dueDate, String priorityLevel) {
+        this.name = name;
+        this.dueDate = dueDate;
+        this.priorityLevel = priorityLevel;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDueDate() {
+        return dueDate;
+    }
+
+    public void setDueDate(String dueDate) {
+        this.dueDate = dueDate;
+    }
+
+    public String getPriorityLevel() {
+        return priorityLevel;
+    }
+
+    public void setPriorityLevel(String priorityLevel) {
+        this.priorityLevel = priorityLevel;
+    }
+
+    @Override
+    public String toString() {
+        // using math.max to calculate padding inbetween the task name and date
+        // so everything is aligned
+        int namePadding = Math.max(0, 20 - name.length());
+
+        return String.format("%s%" + namePadding + "s\t%s\t%s", name, "", dueDate, priorityLevel);
+    }
+}
